f13dd2c26ff1e2f8e4636146e26888254888d305
Nishant Poonia <nishant.poonia6396@gmail.com>
2024-06-22 18:25:37 +0530

    frontend deployment changes

--- issue_tracker_frontend/src/api/axiosInstance.ts
+++ issue_tracker_frontend/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost/api';
+const API_URL = 'https://issue-tracker-backend-zfkld7msdq-uc.a.run.app/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
