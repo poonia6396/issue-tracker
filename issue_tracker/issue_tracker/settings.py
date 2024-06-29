6c0b7918c64665215ef0de4bd979ab910828702e
Nishant Poonia <nishant.poonia6396@gmail.com>
2024-06-29 15:04:12 +0530

    Update settings.py

--- issue_tracker/issue_tracker/settings.py
+++ issue_tracker/issue_tracker/settings.py
@@ -91,8 +91,11 @@ WSGI_APPLICATION = 'issue_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
     }
 }
 
